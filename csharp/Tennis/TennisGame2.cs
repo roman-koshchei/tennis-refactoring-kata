9bfe43b871f6ccc5bfeca36ed6e7d24f452328ae
Roman Koshchei <romankoshchei@gmail.com>
2024-02-06 13:41:12 +0200

    looks great

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -5,9 +5,6 @@ namespace Tennis
         private int p1point = 0;
         private int p2point = 0;
 
-        private string p1res = "";
-        private string p2res = "";
-
         public TennisGame2(string player1Name, string player2Name)
         {
             // player names aren't used, but keep to not break tests
@@ -17,45 +14,28 @@ namespace Tennis
 
         public string GetScore()
         {
-            var score = "";
             if (p1point == p2point)
             {
                 return p1point switch
                 {
-                    0 => "Love-All",
-                    1 => "Fifteen-All",
-                    2 => "Thirty-All",
+                    <= 2 => $"{Count(p1point)}-All",
                     _ => "Deuce"
                 };
             }
 
             if (p1point < 4 && p2point < 4)
             {
-                p1res = Count(p1point);
-                p2res = Count(p2point);
-                return $"{p1res}-{p2res}";
+                return $"{Count(p1point)}-{Count(p2point)}";
             }
 
-            if (p1point >= 4 && (p1point - p2point) >= 2)
+            var difference = p1point - p2point;
+            return difference switch
             {
-                return "Win for player1";
-            }
-            if (p2point >= 4 && (p2point - p1point) >= 2)
-            {
-                return "Win for player2";
-            }
-
-            if (p1point > p2point)
-            {
-                return "Advantage player1";
-            }
-
-            if (p2point > p1point)
-            {
-                return "Advantage player2";
-            }
-
-            return score;
+                >= 2 => "Win for player1",
+                <= -2 => "Win for player2",
+                > 0 => "Advantage player1",
+                _ => "Advantage player2"
+            };
         }
 
         private static string Count(int points)
