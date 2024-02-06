6414265bf8ed3207a35219ec1123153c79793cb0
Roman Koshchei <romankoshchei@gmail.com>
2024-02-06 13:14:13 +0200

    close to good looking

--- csharp/Tennis.Tests/TennisTests.cs
+++ csharp/Tennis.Tests/TennisTests.cs
@@ -74,6 +74,7 @@ namespace Tennis.Tests
             var game = new TennisGame3("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
+
         [Theory]
         [ClassData(typeof(TestDataGenerator))]
         public void Tennis4Test(int p1, int p2, string expected)
@@ -81,6 +82,7 @@ namespace Tennis.Tests
             var game = new TennisGame4("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
+
         [Theory]
         [ClassData(typeof(TestDataGenerator))]
         public void Tennis5Test(int p1, int p2, string expected)
@@ -88,6 +90,7 @@ namespace Tennis.Tests
             var game = new TennisGame5("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
+
         [Theory]
         [ClassData(typeof(TestDataGenerator))]
         public void Tennis6Test(int p1, int p2, string expected)
@@ -95,6 +98,7 @@ namespace Tennis.Tests
             var game = new TennisGame6("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
+
         private void CheckAllScores(ITennisGame game, int player1Score, int player2Score, string expectedScore)
         {
             var highestScore = Math.Max(player1Score, player2Score);
